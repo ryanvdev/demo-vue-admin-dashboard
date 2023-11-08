d44a1cca70839d98eb02cd42dc45a5971927272d
tuannguyen <tuannguyen7326.dev@gmail.com>
2023-11-09 03:40:10 +0700

    v0.0.0

---
+++ docs/assets/VContainer-9544869a.js
@@ -0,0 +1,1 @@
+import{p as n,a8 as o,ac as r,n as l,aq as c,w as i,x as u}from"./index-29494f0e.js";const m=n({fluid:{type:Boolean,default:!1},...o(),...r()},"VContainer"),d=l()({name:"VContainer",props:m(),setup(e,a){let{slots:s}=a;const{rtlClasses:t}=c();return i(()=>u(e.tag,{class:["v-container",{"v-container--fluid":e.fluid},t.value,e.class],style:e.style},s)),{}}});export{d as V};
